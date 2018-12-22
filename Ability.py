d98c9fbd8378039da6d3bbb97ff4ad8c81334335
runningfugitive3 <runningfugitive3@gmail.com>
2018-12-22 09:29:10 -0600

    made roll a func

--- Ability.py
+++ Ability.py
@@ -3,24 +3,26 @@ from Dice import Dice
 class Ability():
     """docstring for Ability."""
     def __init__(self, d=None, roll=False):
-        if not roll:
-            if d:
-                self.strength = d['strength']
-                self.dexterity = d['dexterity']
-                self.constitution = d['constitution']
-                self.intelligence = d['intelligence']
-                self.wisdom = d['wisdom']
-                self.charisma = d['charisma']
+        if d:
+            self.strength = d['strength']
+            self.dexterity = d['dexterity']
+            self.constitution = d['constitution']
+            self.intelligence = d['intelligence']
+            self.wisdom = d['wisdom']
+            self.charisma = d['charisma']
 
-            if not d:
-                self.strength = [0]
-                self.dexterity = [0]
-                self.constitution = [0]
-                self.intelligence = [0]
-                self.wisdom = [0]
-                self.charisma = [0]
+        if not d:
+            self.strength = [0]
+            self.dexterity = [0]
+            self.constitution = [0]
+            self.intelligence = [0]
+            self.wisdom = [0]
+            self.charisma = [0]
 
         if roll:
+            roll()
+
+    def roll(self):
             roll = Dice('character').throw()
             self.strength = [roll[0]]
             self.dexterity = [roll[1]]
